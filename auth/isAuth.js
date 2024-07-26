4b553d6dd61871467d29b5bbb8e36a2bbcfe4119
surya-10 <sp659151@gmail.com>
2024-07-26 07:36:06 +0530

    second commit

--- auth/isAuth.js
+++ auth/isAuth.js
@@ -10,8 +10,9 @@ export function isAuth(req, res, next){
             return res.status(400).send({ response: "Authentication failed", ok:false });
         }
         let result = jwt.verify(token, process.env.SECRET_KEY);
+        console.log(result);
         next();
     } catch (error) {
-        return res.status(500).json({token:null, response:"no token", ok:false})
+        return res.status(500).json({token:null, response:"no token", ok:false, error:error})
     }
 }
